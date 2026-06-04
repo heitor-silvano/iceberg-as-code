13cf38e7ea4acfa8d71b9cfba0eaa352080f258a
Heitor Silvano <heitor.nunes182@hotmail.com>
2026-06-04 03:03:20 -0300

    feat: installing dependencies

--- app/app.vue
+++ app/app.vue
@@ -1,6 +1,8 @@
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
-  </div>
+  <MonacoEditor v-model="value" lang="markdown" :style="{ width: '100%', height: '200px' }" :options="{ theme: 'vs-dark' }"/>
 </template>
+
+<script lang="ts" setup>
+import { ref } from 'vue'
+const value = ref('')
+</script>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,13 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  modules: ['@nuxtjs/tailwindcss', 'nuxt-monaco-editor'],
+  monacoEditor: {
+    locale: 'en',
+    componentName: {
+      codeEditor: 'MonacoEditor',
+      diffEditor: 'MonacoDiffEditor'
+    }
+  }
 })
